2aa407c31eeb861bb688fabd67a24ab16c6e4616
shreeshyamevents <shreeshyameventsharda@gmail.com>
2026-08-12 14:47:55 +0530

    Update app.py

--- aviation/app.py
+++ aviation/app.py
@@ -35,3 +35,8 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == '__main__':
+    # Binds to Render's dynamic PORT variable (defaults to 5000 locally)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
